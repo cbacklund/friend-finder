848f222446bf5a3ada42b14f96a1cbf0f4b1d037
Craig Backlund <craig.backlund@gmail.com>
2018-06-13 09:01:24 -0500

    updating all files, the modal needs some work still

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # friend-finder
+
+## Need some friends? Come get some! 
+
+This application requires you to answer some questions, and then will line you up with the person who is most like you. Opposites don't attract! Apparently clones do! 
---
+++ app/data/friends.js
@@ -0,0 +1,103 @@
+
+
+var userData = [
+  {
+    "name": "Ahmed",
+    "photo": "app/data/images/cool.dude.jpg",
+    "scores":[
+        5,
+      	1,
+      	4,
+      	4,
+      	5,
+      	1,
+      	2,
+      	5,
+      	4,
+      	1
+    ]
+  },
+  /* {
+    "name": "Jimmy",
+    "photo": "../images/beard.dude.jpg",
+    "scores":[
+        3,
+        2,
+        3,
+        4,
+        1,
+        4,
+        5,
+        3,
+        3,
+        2
+    ]
+  },
+  {
+    "name": "Jerome",
+    "photo": "../images/blonde.guy.jpg",
+    "scores":[
+        5,
+        2,
+        3,
+        4,
+        2,
+        2,
+        2,
+        5,
+        1,
+        1
+    ]
+  },
+  {
+    "name": "Chad",
+    "photo": "../images/web.developer.jpg",
+    "scores":[
+        4,
+        4,
+        4,
+        4,
+        5,
+        5,
+        5,
+        2,
+        1,
+        1
+    ]
+  },
+  {
+    "name": "Hope",
+    "photo": "../images/millennial.girl.jpg",
+    "scores":[
+        3,
+        3,
+        3,
+        1,
+        1,
+        2,
+        2,
+        1,
+        3,
+        1
+    ]
+  },
+  {
+    "name": "Jane",
+    "photo": "../images/redhead.girl.jpg",
+    "scores":[
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1,
+        1
+    ]
+  } */
+];
+
+// export the array here
+module.exports = userData;
